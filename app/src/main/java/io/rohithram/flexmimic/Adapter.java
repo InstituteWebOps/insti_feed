9f371533bbdbc5d9e4afa87907f8b81253f124b0
rohithram <ram234historyalex@gmail.com>
2017-06-21 16:06:48 +0530

    Added instifeed activity and its ui

---
+++ app/src/main/java/io/rohithram/flexmimic/Adapter.java
@@ -0,0 +1,120 @@
+package io.rohithram.flexmimic;
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by rohithram on 14/6/17.
+ */
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>  {
+    Context context;
+    List<String> cardcontent;
+    List<String> carddate;
+    List<String> cardtime;
+    List<String> cardloc;
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.content_main, parent, false);
+        return new ViewHolder(itemView);
+    }
+    public Adapter(Context context, List<String> cardcontent,List<String> carddate,List<String> cardtime,List<String> cardloc){
+        this.context = context;
+        this.cardcontent = cardcontent;
+        this.carddate = carddate;
+        this.cardtime = cardtime;
+        this.cardloc = cardloc;
+
+    }
+
+    @Override
+    public void onBindViewHolder(final Adapter.ViewHolder holder, int position) {
+        holder.tv_maindate.setText(carddate.get(holder.getAdapterPosition()));
+        holder.tv_briefdes.setText(cardcontent.get(holder.getAdapterPosition()));
+        holder.tv_date.setText(carddate.get(holder.getAdapterPosition()));
+        holder.tv_time.setText(cardtime.get(holder.getAdapterPosition()));
+        holder.tv_loc.setText(cardloc.get(holder.getAdapterPosition()));
+
+
+
+        holder.cv_content.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.tv_des.getVisibility() == View.GONE) {
+                    // it's collapsed - expand it
+                    holder.tv_des.setVisibility(View.VISIBLE);
+                    holder.bt_show.setImageResource(R.drawable.ic_expand_less_black_24dp);
+                } else {
+                    // it's expanded - collapse it
+                    holder.tv_des.setVisibility(View.GONE);
+                    holder.bt_show.setImageResource(R.drawable.ic_expand_more_black_24dp);
+                }
+
+                ObjectAnimator animation = ObjectAnimator.ofInt(holder.tv_des, "maxLines",holder.tv_des.getMaxLines());
+                animation.setDuration(200).start();
+            }
+        });
+        holder.bt_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.tv_des.getVisibility() == View.GONE) {
+                    // it's collapsed - expand it
+                    holder.tv_des.setVisibility(View.VISIBLE);
+                    holder.bt_show.setImageResource(R.drawable.ic_expand_less_black_24dp);
+                } else {
+                    // it's expanded - collapse it
+                    holder.tv_des.setVisibility(View.GONE);
+                    holder.bt_show.setImageResource(R.drawable.ic_expand_more_black_24dp);
+                }
+
+                ObjectAnimator animation = ObjectAnimator.ofInt(holder.tv_des, "maxLines",holder.tv_des.getMaxLines());
+                animation.setDuration(200).start();
+            }
+        });
+
+    }
+
+
+            @Override
+    public int getItemCount() {
+        return cardcontent.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public CardView cv_content;
+        public TextView tv_maindate;
+        public TextView tv_briefdes;
+        public TextView tv_des;
+        public TextView tv_time;
+        public TextView tv_loc;
+        public TextView tv_date;
+        ImageButton bt_share,bt_save,bt_show,bt_sched,bt_contact;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            cv_content = (CardView)itemView.findViewById(R.id.cv_content);
+            tv_maindate = (TextView)itemView.findViewById(R.id.tv_maindate);
+            tv_briefdes =(TextView)itemView.findViewById(R.id.tv_briefdes);
+            tv_des = (TextView)itemView.findViewById(R.id.tv_des);
+            tv_date = (TextView)itemView.findViewById(R.id.tv_date);
+            tv_time = (TextView)itemView.findViewById(R.id.tv_time);
+            tv_loc = (TextView)itemView.findViewById(R.id.tv_loc);
+
+            bt_share = (ImageButton)itemView.findViewById(R.id.bt_share);
+            bt_save = (ImageButton)itemView.findViewById(R.id.bt_save);
+            bt_sched = (ImageButton)itemView.findViewById(R.id.bt_sched);
+            bt_contact = (ImageButton)itemView.findViewById(R.id.bt_contact);
+            bt_show = (ImageButton)itemView.findViewById(R.id.bt_show);
+
+
+        }
+    }
+}
---
+++ app/src/main/java/io/rohithram/flexmimic/InstiFeed.java
@@ -0,0 +1,102 @@
+package io.rohithram.flexmimic;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class InstiFeed extends AppCompatActivity {
+    RecyclerView rv_list;
+    CardView cv_content;
+    TextView tv_maindate;
+    TextView tv_briefdes;
+    TextView tv_des;
+    Adapter customAdapter;
+    List<String> cardcontent;
+    List<String> carddate;
+    List<String> cardtime;
+    List<String> cardloc;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main1);
+
+        rv_list = (RecyclerView)findViewById(R.id.rv_list);
+        rv_list.setItemAnimator(new DefaultItemAnimator());
+        rv_list.setLayoutManager(new LinearLayoutManager(this));
+
+        tv_briefdes = (TextView)findViewById(R.id.tv_briefdes);
+        tv_maindate = (TextView)findViewById(R.id.tv_maindate);
+        tv_des = (TextView)findViewById(R.id.tv_des);
+        cv_content = (CardView)findViewById(R.id.cv_content);
+
+        cardcontent = new ArrayList<>();
+        carddate = new ArrayList<>();
+        cardtime = new ArrayList<>();
+        cardloc = new ArrayList<>();
+
+        carddate.add("Thurs Jun 15,2017");
+        carddate.add("Fri Jun 16,2017");
+        carddate.add("Sat Jun 17,2017");
+        carddate.add("Mon Jun 19,2017");
+        carddate.add("Tues Jun 20,2017");
+        carddate.add("Wed Jun 21,2017");
+
+        cardtime.add("10.00 AM");
+        cardtime.add("11.00 AM");
+        cardtime.add("5.00 PM");
+        cardtime.add("10.00 AM");
+        cardtime.add("4.00 pM");
+        cardtime.add("10.00 AM");
+
+        cardloc.add("SAC");
+        cardloc.add("CLT");
+        cardloc.add("CRC 101");
+        cardloc.add("HSB Hall");
+        cardloc.add("ICSR");
+        cardloc.add("CLT");
+
+
+        cardcontent.add("UG/DD:TUM Summer School 'Engineering and Geodesy'");
+        cardcontent.add("Mentor For Interns (MFI) 2017");
+        cardcontent.add("Call for applications ||| Saathi Events Coordinators 2017-18");
+        cardcontent.add("Atma Yoga classes");
+        cardcontent.add("CSIE IIT Madras is Hiring - Call for Applications 2017-18");
+        cardcontent.add("Endsem collection drive");
+
+
+
+
+
+        customAdapter = new Adapter(this,cardcontent,carddate,cardtime,cardloc);
+        rv_list.setAdapter(customAdapter);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.spinner, menu);
+
+        MenuItem item = menu.findItem(R.id.spinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_list_item_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+        return true;
+    }
+}
